8e2ddc6e1dab0895e1465d642e9f73aa45120e07
minomura <naotoitoi@gmail.com>
2025-01-23 17:55:47 +0900

    add logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,8 @@ import React, { ReactElement } from "react";
 
 function Header(): ReactElement {
   return (
-    <header className="w-full bg-bg-main text-text-main text-center p-5 m-5 text-lg font-bold">
+    <header className="w-full bg-bg-main text-text-main text-center p-5 mb-5 text-lg font-bold flex items-center justify-center">
+      <img src="/logo.svg" alt="momen.ai" className="w-5 h-5 mr-2 mt-0.5" />
       momen.ai
     </header>
   );
